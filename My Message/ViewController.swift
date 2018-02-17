8842bfde4d97b9fc6c89f060ac588e7798d46f06
Nikola1993 <31863147+Nikola1993@users.noreply.github.com>
2018-02-17 13:50:32 +0100

    Initial Commit

---
+++ My Message/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  My Message
+//
+//  Created by Nikola Popovic on 2/17/18.
+//  Copyright © 2018 Nikola Popovic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
